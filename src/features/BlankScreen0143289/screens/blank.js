12ca3d9fbae63490a27a2853f77bdd1ecd4ceec6
Crowdbotics <team@crowdbotics.com>
2020-10-15 13:03:05 +0000

    Update components

--- src/features/BlankScreen0143289/screens/blank.js
+++ src/features/BlankScreen0143289/screens/blank.js
@@ -26,9 +26,15 @@ export default class Blank extends React.Component {
   state = {}
 
   render = () => (
-    <View style={styles.View_1}>
+    <ImageBackground
+      source={{
+        uri:
+          "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/logo%E6%8B%B7%E8%B2%9D_u1zRtRD.png"
+      }}
+      style={styles.ImageBackground_1}
+    >
       <Image resizeMode="cover" style={styles.Image_3} />
-    </View>
+    </ImageBackground>
   )
 }
 
@@ -37,7 +43,58 @@ const styles = StyleSheet.create({
     flex: 1,
     marginHorizontal: 16
   },
+
   View_1: {
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 0,
+    marginBottom: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingTop: 0,
+    paddingBottom: 0,
+    overflow: "visible",
+    fontSize: 12,
+    color: "#000000",
+    backgroundColor: "#ffffff",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    flexDirection: "column",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
+    alignItems: "stretch",
+    alignContent: "stretch",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0
+  },
+
+  Image_3: {
+    height: 200,
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 0,
+    marginBottom: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingTop: 0,
+    paddingBottom: 0,
+    overflow: "visible",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0
+  },
+  ImageBackground_1: {
     marginLeft: 0,
     marginRight: 0,
     marginTop: 0,
